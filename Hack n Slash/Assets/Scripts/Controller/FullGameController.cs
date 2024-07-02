d716dac6247894c7faf4589892fb43b82e9a4894
SuryaSavana <ssavanakw@gmail.com>
2024-07-03 05:55:20 +0700

    scene transition and health bar
    making scene transition
    player health bar attached

--- Hack n Slash/Assets/Scripts/Controller/FullGameController.cs
+++ Hack n Slash/Assets/Scripts/Controller/FullGameController.cs
@@ -6,17 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class FullGameController : MonoBehaviour
 {
-    // Variables
-    public int score;
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI waveTitle;
-    public static GameController gC; // Creates a singleton class for GameController
-    private float musicLoopTimer;
-    public GameObject popUpHelp;
-    public GameObject credits;
-    public float gameTimer;
-    public TextMeshProUGUI gameTimerTMP = null;
-    public bool waveSoundPlayed;
 
     // Start is called before the first frame update
     void Start()
---
+++ Hack n Slash/Assets/Scripts/Controller/SceneThing.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneThing : MonoBehaviour
+{
+    [SerializeField] private GameObject _startingSceneTransition;
+    [SerializeField] private GameObject _endingSceneTransition;
+    [SerializeField] private int nextSceneBuildIndex;
+
+    private void Start()
+    {
+        _startingSceneTransition.SetActive(true);
+        Invoke("DisableStartingSceneTransition", 3f);
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void DisableStartingSceneTransition()
+    {
+        _startingSceneTransition.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        _endingSceneTransition.SetActive(true);
+        Invoke("DisableEndingSceneTransition", 5f);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextSceneBuildIndex);
+    }
+
+    private void DisableEndingSceneTransition()
+    {
+        _endingSceneTransition.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    // Method to set next scene build index
+    public void SetNextSceneBuildIndex(int index)
+    {
+        nextSceneBuildIndex = index;
+    }
+}
